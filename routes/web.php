ac0c7d61c09c3cad7546e42c2c8029eb2ff3a55f
jiejia2009 <jiejia2009@gmail.com>
2020-11-13 17:34:58 +0800

    '生成话题骨架'

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,5 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 // 用户个人
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
