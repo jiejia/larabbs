f4c131aeeb2743f52a70d0ebb36a9db735c654d4
jiejia2009 <jiejia2009@gmail.com>
2020-11-15 12:06:22 +0800

    填充用户和话题数据

--- database/migrations/2020_11_13_170431_create_categories_table.php
+++ database/migrations/2020_11_13_170431_create_categories_table.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateCategoriesTable extends Migration
 {
-    /**
+    /**w
      * Run the migrations.
      *
      * @return void
@@ -19,6 +20,26 @@ class CreateCategoriesTable extends Migration
             $table->text('description')->nullable()->comment('描述');
             $table->integer('post_count')->default(0)->comment('帖子数');
         });
+
+        $categories = [
+            [
+                'name' => '分享',
+                'description' => '分享创造，分享发现',
+            ],
+            [
+                'name' => '教程',
+                'description' => '开发技巧、推荐扩展包等',
+            ],
+            [
+                'name' => '问答',
+                'description' => '请保持友善，互帮互助',
+            ],
+            [
+                'name' => '公告',
+                'description' => '站点公告',
+            ],
+        ];
+        DB::table('categories')->insert($categories);
     }
 
     /**
