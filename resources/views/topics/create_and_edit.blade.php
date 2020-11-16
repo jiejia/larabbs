51143647e3c8a30bd49fbe4ecaafc7d40b74a9ed
jiejia2009 <jiejia2009@gmail.com>
2020-11-16 10:51:24 +0800

    '添加编辑器'

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,4 +1,8 @@
-@extends('layouts.app') @section('content')
+@extends('layouts.app')
+@section('styles')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
+@section('content')
   <div class="container">
     <div class="col-md-10 offset-md-1">
       <div class="card ">
@@ -48,3 +52,17 @@
  </div>
  </div>
 @endsection
+
+@section('scripts')
+  <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+  <script>
+    $(document).ready(function() {
+      var editor = new Simditor({
+        textarea: $('#editor'),
+      });
+    });
+  </script>
+@stop
